a5203d35d636134e4b33e7aea270b0e8cb4c9322
Leonardo <leonardo38361@gmail.com>
2026-01-16 17:27:59 -0300

    Commit: src/products/index.ts

--- src/products/index.ts
+++ src/products/index.ts
@@ -7,9 +7,10 @@ export { ProductList } from './components/ProductList';
 
 export { StoreLayout } from './layout/StoreLayout';
 export { useProducts } from './hooks/useProducts';
+export { useProduct } from './hooks/useProduct';
 
 export type { Product } from './interfaces/products';
-
+export { ProductById } from './pages/ProductById';
 
 export { CompleteListPage } from './pages/CompleteListPage';
 export { MensPage } from './pages/MensPage';
