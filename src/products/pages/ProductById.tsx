431b4a8832a6974f5a48ebc86dfd46d691ab60af
Leonardo <leonardo38361@gmail.com>
2026-01-16 17:34:49 -0300

    feat(products): update ProductById page

--- src/products/pages/ProductById.tsx
+++ src/products/pages/ProductById.tsx
@@ -1,12 +1,17 @@
 import { useParams } from "react-router-dom"
 import { useProduct } from "../hooks/useProduct"
 import { ProductCard } from "../components/ProductCard"
+import { useEffect } from "react"
 
 
 export const ProductById = () => {
     const { id } = useParams()
 
-    const { product, isLoading } = useProduct({ id: +id! })
+    const { product, isLoading } = useProduct({ id: +id! });
+
+    useEffect(()=>{
+        window.scrollTo(0,0);
+    })
 
     return (
         <div className="flex-col">
