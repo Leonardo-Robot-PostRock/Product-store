9eaaaed8397a0183a555ef407ef5c5a11a213c21
Leonardo <leonardo38361@gmail.com>
2026-01-16 18:03:47 -0300

    Commit: src/products/index.ts

--- src/products/index.ts
+++ src/products/index.ts
@@ -8,6 +8,7 @@ export { ProductList } from './components/ProductList';
 export { StoreLayout } from './layout/StoreLayout';
 export { useProducts } from './hooks/useProducts';
 export { useProduct } from './hooks/useProduct';
+export { usePrefetchProduct } from './hooks/usePrefetchProduct';
 
 export type { Product } from './interfaces/products';
 export { ProductById } from './pages/ProductById';
