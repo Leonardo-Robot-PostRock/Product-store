a48eb4d60485e6a43690bd65183bff21151cea12
Leonardo <leonardo38361@gmail.com>
2026-01-16 18:03:47 -0300

    Commit: src/products/components/ProductList.tsx

--- src/products/components/ProductList.tsx
+++ src/products/components/ProductList.tsx
@@ -1,17 +1,22 @@
-import { Product, ProductCard } from ".."
+import { Product, ProductCard, usePrefetchProduct } from ".."
 
 interface Props {
   products: Product[];
 }
 
 export const ProductList = ({ products }: Props) => {
+
+  const prefetchProduct = usePrefetchProduct()
+
   return (
     <div className="mt-2 columns-1 sm:columns-2 md:columns-3 xl:columns-4 masonry">
-      {products.map((product: Product) => (
-        <div key={product.id} className="masonry-item">
-          <ProductCard product={product} />
-        </div>
-      ))}
+      {
+        products.map((product: Product) => (
+          <div key={product.id} className="masonry-item">
+            <ProductCard product={product} key={product.id} prefetchProduct={prefetchProduct} />
+          </div>
+        ))
+      }
     </div>
   )
 }
