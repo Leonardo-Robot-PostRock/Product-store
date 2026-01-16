6768ab4b7bea68fbc513f8de0b26189391da1a91
Leonardo <leonardo38361@gmail.com>
2026-01-16 18:03:47 -0300

    Commit: src/products/hooks/usePrefetchProduct.tsx

---
+++ src/products/hooks/usePrefetchProduct.tsx
@@ -0,0 +1,12 @@
+import { useQueryClient } from "@tanstack/react-query"
+import { productActions } from ".."
+
+export const usePrefetchProduct = () => {
+    const queryClient = useQueryClient()
+
+    const prefetchProduct = (id: number) => {
+        queryClient.prefetchQuery({ queryKey: ['product', id] }), async () => productActions.getProductById(id)
+    }
+
+    return prefetchProduct
+}
